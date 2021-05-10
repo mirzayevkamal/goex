1c3eab69ecf3df87a232d7fe379f67e80d394829
Kamal <mirzayevkamal@gmail.com>
2021-05-11 01:05:56 +0400

    finished main page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(".hamburger-menu").on("click", function () {
 });
 
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
+  $(".main-carousel .owl-carousel").owlCarousel({
     autoplay: true,
     autoplayTimeout: 8000,
     animateOut: "fadeOut",
@@ -23,6 +23,62 @@ $(document).ready(function () {
       },
       1000: {
         items: 1,
+        nav: false,
+        loop: true,
+      },
+    },
+  });
+});
+
+$(document).ready(function () {
+  $(".shops-carousel .owl-carousel").owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 8000,
+    animateOut: "fadeOut",
+    margin: 10,
+    responsiveClass: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 2,
+        nav: false,
+        loop: true,
+      },
+      600: {
+        items: 4,
+        nav: false,
+        loop: true,
+      },
+      1000: {
+        items: 6,
+        nav: false,
+        loop: true,
+      },
+    },
+  });
+});
+
+$(document).ready(function () {
+  $(".partners-carousel .owl-carousel").owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 8000,
+    animateOut: "fadeOut",
+    margin: 10,
+    responsiveClass: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 2,
+        nav: false,
+        loop: true,
+      },
+      600: {
+        items: 4,
+        nav: false,
+        loop: true,
+      },
+      1000: {
+        items: 6,
         nav: false,
         loop: true,
       },
